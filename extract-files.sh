f2b6f9518ff3075c178dba83558c9ace772e6c55
LuK1337 <priv.luk@gmail.com>
2020-10-13 02:49:28 +0200

    odessa: Insert qtimutex.so dependency into camera hal
    * Fixes missing __floatditf / __subtf3 symbol.
    
    Change-Id: Ic2b87ad336ad47d030ba0c529eeb9f08406ba5c5

--- extract-files.sh
+++ extract-files.sh
@@ -19,3 +19,12 @@ export DEVICE_COMMON=sm6150-common
 export VENDOR=motorola
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+ANDROID_ROOT="$MY_DIR"/../../..
+BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+CAMERA_HAL_CHI="$BLOB_ROOT"/vendor/lib64/hw/com.qti.chi.override.so
+sed -i "s/libhidltransport.so/qtimutex.so\x00\x00\x00\x00\x00\x00\x00\x00/" "$CAMERA_HAL_CHI"
