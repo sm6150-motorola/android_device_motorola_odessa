68a2293386741fa24af4cdbfbb1c39bed64d721d
Erfan Abdi <erfangplus@gmail.com>
2021-03-29 20:11:19 +0430

    odessa: Drop FOD and 90hz overlays and props

--- properties.mk
+++ properties.mk
@@ -6,30 +6,13 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.physical.num=4
 
-# Fingerprint
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.fingerprint=goodix
-
 # Firmware
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.hw.modem_version=.
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.sf.color_mode=0 \
-    persist.sys.sf.enable_refresh_rate_rectification=0 \
-    persist.sys.sf.set_rectify_timer_ms=1000 \
-    persist.sys.sf.fps_threshold_duration_ms=290 \
-    persist.sys.sf.fps_threshold_count=29 \
-    persist.sys.sf.brightness_threshold_refresh_rate=6 \
-    persist.sys.sf.forbidden_RRR_under_lowbrightness=1
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=4000 \
-    ro.surface_flinger.set_touch_timer_ms=4000 \
-    ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.use_smart_90_for_video=true \
-    ro.surface_flinger.refresh_rate_switching=true \
+    persist.sys.sf.color_mode=0
 
 # HDR
 PRODUCT_PROPERTY_OVERRIDES += \
